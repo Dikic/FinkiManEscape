a273a3b14d20207160b27b11d8776b122d79b64e
Dikic <dikic1992@gmail.com>
2013-05-01 00:45:25 +0200

    FirstCommit
    napraveni klase za figuru ednu i za igru i nekoi funkcie

---
+++ FinkiManEscape/Figura.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace FinkiManEscape
+{
+    class Figura
+    {
+        public int Lenth { get; set; }//kolko kocke da zafati
+
+        public int PositionX { get; set; }//pozicija kude pocnue figura(kolona)
+        public int PositionY { set; get; }//pozicija kude pocnue figura(red)
+
+        public int Orinetation { get; set; }//ovoj e jasno :P
+        static readonly int PORTRAIT = 0;
+        static readonly int LANDSCAPE = 1;
+        
+        public int[] Bounds { set; get; }//Slobodni kocke bounds[0]:UP bounds[1]:DOWN...
+        static readonly int BOUNDUP = 0;
+        static readonly int BOUNDDOWN = 1;
+        static readonly int BOUNDLEFT = 2;
+        static readonly int BOUNDRIGHT = 3;
+
+        public Figura(int lenth, int positionX, int positionY, int orientation)
+        {
+            Lenth = lenth;
+            PositionX = positionX;
+            PositionY = positionY;
+            Orinetation = orientation;
+            Bounds = new int[4];
+        }
+
+        public bool move(int moveLenth)
+        {
+            if (Orinetation == PORTRAIT)
+            {
+                //ispitaj dali e do kraj
+                PositionY += moveLenth;
+            }
+            else
+            {
+                //ispitaj granice
+                PositionX += moveLenth;
+            }
+            return true;
+        }
+
+        public void adjust()
+        {
+            //pri mouseUp treba da gu namesti figuru na mesto
+        }
+
+        public void draw(Graphics g)
+        {
+            //iscrtaj figuru
+        }
+       
+    }
+}
---
+++ FinkiManEscape/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FinkiManEscape
+{
+    class Game
+    {
+        private Figura[] figuri;
+
+        public bool[][] Grid;//koja kocka e slobodna a koja ne 6x6
+
+        static readonly int squareDimension = 100;//pikseli za duzinu i sirinu na ednu kocku
+    }
+}
