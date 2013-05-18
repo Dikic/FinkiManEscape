825419a4c8e8b28817da126f629a9443c6e79351
Jovanovska Mila <succubus.mila@gmail.com>
2013-05-18 21:13:04 +0200

    slike

---
+++ FinkiManEscape/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FinkiManEscape
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
